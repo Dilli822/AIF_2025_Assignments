b29d4ecc8232e302fc71bf48e719b09960e20879
dilli hang rai <dillihangrae@gmail.com>
2025-05-15 15:03:57 +0545

    model imported

--- healthapp/app/ml_model.py
+++ healthapp/app/ml_model.py
@@ -1,8 +1,8 @@
-# import joblib
+import joblib
 
-# model_path = "../models/disease_predict.joblib"
-# # Load the model
-# model = joblib.load(model_path)
+model_path = "../models/disease_predict.joblib"
+# Load the model
+model = joblib.load(model_path)
 
-# print("Model loaded successfully!")
-# print(model)
+print("Model loaded successfully!")
+print(model)
