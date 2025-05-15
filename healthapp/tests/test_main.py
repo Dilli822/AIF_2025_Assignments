53439ccc0d17a4860745d462f2c7de279a9df012
dilli hang rai <dillihangrae@gmail.com>
2025-05-15 08:25:55 +0545

    ... App ....

--- healthapp/tests/test_main.py
+++ healthapp/tests/test_main.py
@@ -1,55 +1,24 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-
-def test_predict_accepts_any_symptoms():
-    sample_data = {
-        "fever": False,
-        "cough": False,
-        "fatigue": True,
-        "headache": True,
-        "skin_rash": False,
-        "itching": False,
-        "dischromic_patches": False,
-    }
-
-    response = client.post("/predict", json=sample_data)
-
-    assert response.status_code == 200
-    assert "prediction" in response.json()
+from app import ml_model
+from app import schemas
+import numpy as np
 
 
-def test_predict_returns_200_with_valid_input():
-    sample_data = {
-        "fever": True,
-        "cough": True,
-        "fatigue": False,
-        "headache": False,
-        "skin_rash": True,
-        "itching": False,
-        "dischromic_patches": True,
-    }
-
-    response = client.post("/predict", json=sample_data)
-
-    assert response.status_code == 200
-    assert "prediction" in response.json()
+class MockSymptoms(schemas.Symptoms):
+    skin_rash: bool = True
+    fatigue: bool = True
+    headache: bool = True
+    vomiting: bool = False
+    cough: bool = False
 
 
-def test_predict_handles_different_symptoms():
-    sample_data = {
-        "fever": True,
-        "cough": False,
-        "fatigue": False,
-        "headache": True,
-        "skin_rash": True,
-        "itching": True,
-        "dischromic_patches": False,
-    }
+def test_preprocessing_vector_shape():
+    input_data = MockSymptoms()
+    vector = ml_model.preprocess_user_input(input_data, ml_model.symptom_columns)
+    assert isinstance(vector, np.ndarray)
+    assert vector.shape == (1, len(ml_model.symptom_columns))
 
-    response = client.post("/predict", json=sample_data)
 
-    assert response.status_code == 200
-    assert "prediction" in response.json()
+def test_predict_returns_string():
+    input_data = MockSymptoms()
+    result = ml_model.predict(input_data)
+    assert isinstance(result, str)
