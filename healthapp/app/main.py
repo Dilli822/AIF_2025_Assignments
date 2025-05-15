3a2711a17ad9aa19235ca54b73334b82fc20a61e
dilli hang rai <dillihangrae@gmail.com>
2025-05-15 14:57:28 +0545

    prediction system updated...

--- healthapp/app/main.py
+++ healthapp/app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from app import schemas, ml_model
 
 app = FastAPI(title="Disease Predictor API")
 
@@ -7,9 +6,3 @@ app = FastAPI(title="Disease Predictor API")
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Disease Predictor API"}
-
-
-@app.post("/predict")
-def predict(input: schemas.Symptoms):
-    prediction = ml_model.predict(input.symptoms)
-    return {"prediction": prediction}
